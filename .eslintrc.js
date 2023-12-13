7e60e2bd6aab7ab3caf3033f99503a42e019dcf5
Fred Cheng <fredcheng@cal.berkeley.edu>
2023-12-13 15:56:05 -0800

    github action fixes: updated eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         "commonjs": true,
         "es2021": true
     },
+    "extends": ["eslint:recommended", "airbnb-base", "prettier"],
     "overrides": [
         {
             "env": {
